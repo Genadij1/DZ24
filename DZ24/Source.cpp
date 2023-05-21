21b033130d5f0fd76ef755e4ee8d6f9a7b0d81af
Геннадий <@DESKTOP-6NN51IE>
2023-05-21 13:11:51 +0300

    ex_2-done

---
+++ DZ24/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << "Enter y: ";
+    cin >> y;
+
+    int result = 1;
+
+    for (int i = 0; i < y; ++i) {
+        result *= x;
+    }
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
